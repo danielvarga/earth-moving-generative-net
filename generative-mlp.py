d2a77f68545bc273d1e88cc04dfc955899fcf415
Daniel Varga <daniel.varga@prezi.com>
2015-10-05 00:18:50 +0200

    some sort of update happening

--- generative-mlp.py
+++ generative-mlp.py
@@ -13,7 +13,7 @@ import lasagne
 import kohonen
 
 def buildNet(input_var, inDim, hidden, outDim):
-    l_in = lasagne.layers.InputLayer(shape=(None, 1, 1, inDim),
+    l_in = lasagne.layers.InputLayer(shape=(None, inDim),
                                      input_var=input_var)
     l_hid = lasagne.layers.DenseLayer(
             l_in, num_units=hidden,
@@ -26,26 +26,44 @@ def buildNet(input_var, inDim, hidden, outDim):
     return l_out
 
 def sampleInitial(n, inDim):
-    return np.random.normal(loc=0.0, scale=1.0, size=(n, 1, 1, inDim))
+    return np.random.normal(loc=0.0, scale=1.0, size=(n, inDim))
 
 def sample(net, n, inDim, input_var):
     initial = sampleInitial(n, inDim)
     output = lasagne.layers.get_output(net)
     net_fn = theano.function([input_var], output)
     return initial, net_fn(initial)
-    # params = lasagne.layers.get_all_params(network, trainable=True)
 
-def learn(data):
+def initialNet():
+    return input_var, net
+
+def plot(input_var, net, name):
+    n = 1000
     inDim = 2
-    outDim = 2
-    hidden = 5
-    assert len(data[0])==outDim
-    input_var = T.tensor4('inputs')
-    net = buildNet(input_var, inDim, hidden, outDim)
-    n = 100
     initial, sampled = sample(net, n, inDim, input_var)
+    assert sampled.shape == (n,2)
     plt.scatter(sampled.T[0], sampled.T[1])
-    plt.savefig("gen.pdf")
+    plt.savefig(name+".pdf")
+
+def update(input_var, net, initial, sampled, data):
+    n = len(data)
+    output = lasagne.layers.get_output(net)
+    data_var = T.matrix('targets')
+    loss = lasagne.objectives.squared_error(output, data_var).mean()
+    params = lasagne.layers.get_all_params(net, trainable=True)
+    updates = lasagne.updates.nesterov_momentum(
+            loss, params, learning_rate=0.05, momentum=0.9)
+    train_fn = theano.function([input_var, data_var], updates=updates)
+    train_fn(initial, data)
+
+def sampleAndUpdate(input_var, net, inDim, n):
+    # TODO Refactor, I can't even change the goddamn target distribution in this source file!
+    data = kohonen.samplesFromTarget(n)
+    initial, sampled = sample(net, n, inDim, input_var)
+    permutation = kohonen.optimalPairing(sampled, data)
+    initial = initial[permutation]
+    sampled = sampled[permutation]
+    update(input_var, net, initial, sampled, data)
 
 def mnist():
     np.random.seed(1)
@@ -62,10 +80,13 @@ def mnist():
 
 
 def main():
-    n = 1000
-    # TODO Refactor, I can't even change the goddamn target distribution in this source file!
-    data = kohonen.samplesFromTarget(n)
-    learn(data)
+    inDim = 2
+    outDim = 2
+    hidden = 5
+    input_var = T.matrix('inputs')
+    net = buildNet(input_var, inDim, hidden, outDim)
+    minibatchSize = 20
+    sampleAndUpdate(input_var, net, inDim, n=minibatchSize)
 
 if __name__ == "__main__":
     main()
